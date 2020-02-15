afb39fa9ccc836031b96c6488efefc6c8a135b26
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-02-15 19:19:52 +0900

    create messages table

---
+++ db/migrate/20200215093329_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.references :group, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.string :content
+      t.string :image
+      t.timestamps
+    end
+  end
+end
