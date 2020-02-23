9a0e5459f7c5efd98b89371a0657cc3283a7421d
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-02-23 17:51:17 +0900

    add auto reloading

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,8 @@
 $(function() {
+  let message_body = $('.main-chat__body');
+
   function buildHTML(message) {
-    let html = `<div class="main-chat__message-items">
+    let html = `<div class="main-chat__message-items" data-message-id="${message.id}">
                   <div class="main-chat__message-items__message-header">
                     <p class="main-chat__message-items__user-name">${message.user_name}</p>
                     <p class="main-chat__message-items__date">${message.created_at}</p>
@@ -13,6 +15,26 @@ $(function() {
     }
     html += `</div>`;
     return html;
+  }
+
+  let reloadMessages = function() {
+    let last_message_id = $('.main-chat__message-items:last').data('message-id');
+    $.ajax({
+      type: 'GET',
+      url: 'api/messages',
+      dataType: 'json',
+      data: {id: last_message_id}
+    }).done(function(messages) {
+      if (messages.length == 0) return;
+      let html = ``;
+      messages.forEach(function(message) {
+        html += buildHTML(message);
+      });
+      message_body.append(html);
+      message_body.animate({scrollTop: message_body[0].scrollHeight});
+    }).fail(function() {
+      alert('error');
+    });
   }
 
   $("#new_message").on("submit", function(e) {
@@ -28,12 +50,15 @@ $(function() {
       contentType: false
     }).done(function(data) {
       let html = buildHTML(data);
-      $(".main-chat__body").append(html);
-      $(".main-chat__body").animate({scrollTop: $(".main-chat__body")[0].scrollHeight});
+      message_body.append(html);
+      message_body.animate({scrollTop: message_body[0].scrollHeight});
       $("form")[0].reset();
       $(".main-chat__form-items__send-btn").prop("disabled", false);
     }).fail(function() {
       alert("メッセージ送信に失敗しました");
     });
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
